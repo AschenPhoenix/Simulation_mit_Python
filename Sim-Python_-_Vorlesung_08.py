e771b6eb2ded770f72d6ae3660640fa847b71911
AschenPhoenix <alex-1106@web.de>
2024-01-21 00:36:02 +0100

    Vorlesung 8

--- Sim-Python_-_Vorlesung_08.py
+++ Sim-Python_-_Vorlesung_08.py
@@ -1,11 +1,11 @@
 #
 #               WiSe 2324 - Simulation technischer Systeme mit Python
-#                               Vorlesung / Übung 00
+#                               Vorlesung 08
 #
 # _______________________________________________________________________
 # --------- Main Bibliotheken --------------
-import math as mp
 import numpy as np
+import math as mp
 import scipy as sc
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
@@ -32,9 +32,20 @@ import Uebung_04_testfunk
 # os.system('clear')
 # os.system('cls')
 print("\n\n\n")
-# ======================================== Teil 00 =================================
-print(f'\n\n=====================\n||   Thema / Aufgabe 0.0   ||\n=====================\n')
+# ======================================== Teil 01 =================================
+print(f'\n\n=====================\n||   Thema 1.0   ||\n=====================\n')
 # ==================================================================================
+polynom1 = np.array([1, 2, -9, -2, 5])  # Definition
+x_min = -5
+x_max = 3
+x = np.linspace(x_min, x_max, 200)
+y = np.polyval(polynom1, x)
+plt.plot(x, y, 'r', label='Polynom 1', linewidth=2)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid()
+plt.legend('best')
+plt.show()
 
 # ======================================== Teil 02 =================================
 print(f'\n\n=======================\n||   Bsp Aufgabe 1   ||\n=======================\n')
