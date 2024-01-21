16f032b9ff3d5223d17afc5ef19d8fada0a945eb
AschenPhoenix <alex-1106@web.de>
2024-01-21 01:36:30 +0100

    Vorlesung 8

--- Sim-Python_-_Vorlesung_08.py
+++ Sim-Python_-_Vorlesung_08.py
@@ -31,8 +31,7 @@ import Uebung_04_testfunk
 # --------- Terminal vorbereiten ------------
 # os.system('clear')
 # os.system('cls')
-print("\n\n\n")
-# ======================================== Teil 01 =================================
+# ================================== nächster Teil =================================
 print(f'\n\n=====================\n||   Thema 1.0   ||\n=====================\n')
 # ==================================================================================
 polynom1 = np.array([1, 2, -9, -2, 5])  # Definition
@@ -48,7 +47,55 @@ plt.grid()
 plt.legend(loc='best')
 plt.show()
 
-# ======================================== Teil 02 =================================
+# ================================== nächster Teil =================================
+print(f'\n\n=====================\n||   Thema 2.0   ||\n=====================\n')
+# ==================================================================================
+
+Pol_roots = np.roots(polynom1)
+y_0 = np.zeros(Pol_roots.shape)
+plt.figure("Roots")
+plt.plot(x, y, label='Polynom 1', linewidth=2)
+plt.plot(Pol_roots, y_0, '*', label='Roots', linewidth=2)
+plt.vlines(Pol_roots, -50, 175, 'g', ':')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid()
+plt.legend(loc='best')
+plt.show()
+
+# ================================== nächster Teil =================================
+print(f'\n\n=====================\n||   Thema 2.0   ||\n=====================\n')
+# ==================================================================================
+
+plt.figure("Ableitung / Integration")
+plt.plot(x, y, label='Polynom 1', linewidth=2)
+plt.plot(Pol_roots, y_0, 'r*', label='Roots', linewidth=2)
+plt.vlines(Pol_roots, -50, 170, 'g', ':')
+
+poly_abl = np.polyder(polynom1)
+poly_int = np.polyint(polynom1)
+print(f'Polynom Ableitung: {poly_abl}')
+print(f'Polynom Integration: {poly_int}')
+
+poly_abl_plot = np.polyval(poly_abl, x)
+poly_int_plot = np.polyval(poly_int, x)
+plt.plot(x, poly_abl_plot, label='Polynom 1 - Ableitung', linewidth=2)
+plt.plot(x, poly_int_plot, label='Polynom 1 - Integration', linewidth=2)
+
+plt.text(-5.4, 200, 'np.polyval(Koeffizienten, x)', fontsize=12)
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid()
+plt.legend(loc='best')
+plt.show()
+
+# ================================== nächster Teil =================================
+print(f'\n\n=====================\n||   Thema 2.0   ||\n=====================\n')
+# ==================================================================================
+print (f'Vorlesung 8 Video weiter bei Minute 30')
+
+# ================================== nächster Teil =================================
 print(f'\n\n=======================\n||   Bsp Aufgabe 1   ||\n=======================\n')
 # ==================================================================================
 
