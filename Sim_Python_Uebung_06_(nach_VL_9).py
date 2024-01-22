94a0210c2b858b32a81b2a202ad6209bc212482e
AschenPhoenix <alex-1106@web.de>
2024-01-22 22:41:20 +0100

    Vorbereitung Uebung 5 + 6

--- Sim_Python_Uebung_06_(nach_VL_9).py
+++ Sim_Python_Uebung_06_(nach_VL_9).py
@@ -1,13 +1,26 @@
-#
-#               WiSe 2324 - Simulation technischer Systeme mit Python
-#                               Übung 06
-#
-#_______________________________________________________________________
+#########################################################################################
+#                                                                                       #
+#                                       WiSe 2324                                       #
+#                       Simulation technischer Systeme mit Python                       #
+#                                       Uebung 06                                       #
+#                                                                                       #
+#########################################################################################
+# |==============| Bibliotheken |==============|
+#                     region
+# |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+
+# |~~~~~~~~~~~~| Main Bibliotheken |~~~~~~~~~~~|
 import numpy as np
+import math as mth
 import scipy as sc
-import matplotlib.pyplot  as plt
+import matplotlib.pyplot as plt
+import matplotlib as mplt
 from matplotlib.ticker import MultipleLocator
-#-----------------------
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+import pytest
+
+# |~~~~~~~~~~| Weitere Bibliotheken |~~~~~~~~~~|
 import os
 import csv
 import sys
@@ -15,26 +28,37 @@ import serial
 from datetime import datetime
 import _thread as thread
 import time
-
+import random
+import openpyxl as opx
+from copy import copy
 from sympy import true
-#-----------------------
-os.system('clear')
+from scipy.interpolate import interp1d
+import scipy.optimize as opt
+# import opencv as cv2
+
+# |~~~~~~~~~~| Terminal vorbereiten |~~~~~~~~~~|
 os.system('cls')
-print("\n\n\n")
-#======================================== Teil 00 =================================
-print(f'\n\n=====================\n||   Aufgabe 0.0   ||\n=====================\n')
-#==================================================================================
+# |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+#                   endregion
+
+# |=======| Import aus anderer Dateien |=======|
+#                    region
+# |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# import
+# |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+#                   endregion
+
+# |=======| Darstellungseinstellungen |========|
+mplt.use('Qt5Agg')
+np.set_printoptions(suppress=True)
+#########################################################################################
+
+# ======================================= nächster Teil =================================
+print(f'\n\n=========================\n||  Aufgabe 0.0  ||\n=========================\n')
+# =======================================================================================
 
 
-
-
-
-
-
-
-
-
-
-#========================== Ende =======================================
-print("\n\n")
-#=======================================================================
+# ======================================== Ende =========================================
+print("                                    \n\n                                        ")
+# =======================================================================================
+# print (f'Weiter bei Aufgabe 0.0')
