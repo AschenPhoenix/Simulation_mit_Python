61e2f72bcd613e8ce6b25f635d03c933c9ed608b
AschenPhoenix <alex-1106@web.de>
2024-01-25 02:28:25 +0100

    Vorlesung 9

--- Sim-Python_-_Vorlesung_09.py
+++ Sim-Python_-_Vorlesung_09.py
@@ -54,13 +54,70 @@ np.set_printoptions(suppress=True)
 #########################################################################################
 
 # ======================================= nächster Teil =================================
-print(f'\n\n=====================\n||  Thema / Aufgabe 0.0  ||\n=====================\n')
+print(f'\n\n===========================\n||  Thema 1 ||\n============================\n')
 # =======================================================================================
+
+print(f'========||  explites Eulerverfahren ||========\n')
+t1 = np.arange(0, 5, 0.01)
+y1 = 2.5*t1**2
+
+plt.figure(1)
+plt.plot(t1,y1)
+plt.show()
+
+# numerische Lösung mit explizitem Euler-Verfahren
+h = 0.5
+t2 = np.arange(0, 5, h)
+y2 = np.zeros(len(t2))
+
+for i in range(1, len(t2)):
+    y2[i] = y2[i-1] + h*5*t2[i-1]
+
+plt.figure(1)
+plt.plot(t1, y1)
+plt.plot(t2, y2, 'o-')
+plt.grid()
+plt.show()
 
 # ====================================== nächster Teil ==================================
 print(f'\n\n=======================\n||   Bsp Aufgabe 1   ||\n=======================\n')
 # =======================================================================================
+t = np.arange(0.0, 5.0, h)
 
+plt.figure()
+plt1 = plt.subplot(121)
+y_ex = np.zeros(len(t))
+y_im = np.zeros(len(t))
+for h in [0.5, 0.2, 0.05, 0.02, 0.001]:
+    y_ex[0] = 1
+    for i in range(1,len(t)):
+        y_ex[i] = y_ex[i - 1] + h * (-5*(y_ex[i-1]-2))
+    plt1.plot(t, y_ex, 'o-', linewidth=2, label=f'h={h} - explizit Euler')
+
+y_ana = 2 - np.exp(-5 * t)
+plt1.plot(t, y_ana, 'b', linewidth=2, label='Analytische Lösung')
+plt1.set_title('Explizite Eulerverfahren')
+plt1.legend(loc='best')
+plt1.grid()
+
+plt2 = plt.subplot(122)
+for h in [0.5, 0.2, 0.05, 0.02, 0.001]:
+    y_im[0] = 1
+    for i in range(1,len(t)):
+        y_im[i] = (y_im[i-1]+10*h)/(1+5*h)
+    plt2.plot(t, y_im, 'o-', linewidth=2, label=f'h={h} - implizit Euler')
+
+y_ana = 2 - np.exp(-5 * t)
+plt2.plot(t, y_ana, 'b', linewidth=2, label='Analytische Lösung')
+plt2.set_title('Implizite Eulerverfahren')
+plt2.legend(loc='best')
+plt2.grid()
+plt.show()
+
+# ======================================= nächster Teil =================================
+print(f'\n\n===========================\n||  Thema 2 ||\n============================\n')
+# =======================================================================================
+print(f"Vorlesung`s Video weiter bei 00:16:00")
 
 # ======================================== Ende =========================================
 print("                                    \n\n                                        ")
